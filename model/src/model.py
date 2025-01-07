4c1196702386eff8cfa1d8393afbeb9ca696a5d6
CatPawsCoder <dariabespal@gmail.com>
2025-01-07 16:56:55 +0400

    Initial commit1

--- model/src/model.py
+++ model/src/model.py
@@ -2,10 +2,28 @@ import pika
 import pickle
 import numpy as np
 import json
+from sklearn.linear_model import LinearRegression
+from sklearn.datasets import load_diabetes
 
-# Загружаем модель
-with open('myfile.pkl', 'rb') as pkl_file:
-    regressor = pickle.load(pkl_file)
+# 1. Обучаем модель, если файл не существует
+try:
+    with open('myfile.pkl', 'rb') as pkl_file:
+        regressor = pickle.load(pkl_file)
+    print("Модель загружена из файла.")
+except FileNotFoundError:
+    print("Файл модели не найден, обучаем новую модель...")
+    
+    # Загружаем датасет
+    X, y = load_diabetes(return_X_y=True)
+    
+    # Обучаем модель
+    regressor = LinearRegression()
+    regressor.fit(X, y)
+    
+    # Сохраняем модель в файл
+    with open('myfile.pkl', 'wb') as pkl_file:
+        pickle.dump(regressor, pkl_file)
+    print("Модель обучена и сохранена в файл myfile.pkl.")
 
 # Подключение к очереди RabbitMQ
 connection = pika.BlockingConnection(pika.ConnectionParameters('rabbitmq'))
@@ -37,3 +55,4 @@ channel.basic_consume(queue='features', on_message_callback=callback, auto_ack=T
 print('Ожидание сообщений...')
 
 channel.start_consuming()
+
